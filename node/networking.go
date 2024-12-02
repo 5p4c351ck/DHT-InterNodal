47f7ce2e0ee622c55c29ea33fb2d5886b3a85578
5p4c351ck <kntazanis@gmail.com>
2024-12-02 02:09:11 +0200

    testing

--- node/networking.go
+++ node/networking.go
@@ -28,6 +28,24 @@ func (node *LocalNode) Server() error {
 	}
 }
 
-func (node *LocalNode) sendRequest() error {
+func (node *LocalNode) sendRequest(m message) error {
+	if m.senderNode == nil || m.receiverNode == nil {
+		return fmt.Errorf("Sender or receiver is nil")
+	}
+	if !m.request {
+		return fmt.Errorf("Message is a reply")
+	}
+	raddr := &net.UDPAddr{
+		IP:   m.receiverNode.IP,
+		Port: m.receiverNode.Port,
+	}
+	conn, err := net.DialUDP(protocol, nil, raddr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	input := []byte("Testing connectivity")
+	_, err = conn.Write(input)
 
 }
